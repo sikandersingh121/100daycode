7ca37b3e40aabf8d140f8af45d6ebbc9949ab0c1
sikandersingh121 <sunnybrar70777@gmail.com>
2026-09-02 16:02:51 +0530

    Add files via upload

---
+++ day_24_Q_1.c
@@ -0,0 +1,23 @@
+// Q47 (Nested Loops without Arrays/Strings)
+// 📋
+// Write a program to print the following pattern:
+// *
+// **
+// ***
+// ****
+// *****
+
+
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+
+    for(int i=1;i<6;i++){
+        for(int j=1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
